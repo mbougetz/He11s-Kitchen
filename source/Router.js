32eb64b594327bc3306a2176ac31ddf2bfed47ba
jingtianlin <linjingtian@easyretailpro.com>
2021-12-05 15:32:56 -0800

    Merge branch 'backend-branch' of https://github.com/jingtianlin19/cse110-sp21-group11 into backend-branch

--- source/Router.js
+++ source/Router.js
@@ -1,19 +1,19 @@
 export class Router {
-  static routes = {};
+    static routes = {};
 
-  constructor(func) {
-    this["home"] = func;
-  }
-
-  setExpand(page, pageFunc) {
-    this[`${page}`] = pageFunc;
-  }
-
-  navigate(page) {
-    if (!this[page]) {
-      window.alert("page does not exist");
-      return;
+    constructor(func) {
+        this['home'] = func;
     }
-    this[page]();
-  }
+
+    setExpand(page, pageFunc) {
+        this[`${page}`] = pageFunc;
+    }
+
+    navigate(page) {
+        if (!this[page]) {
+            window.alert('page does not exist');
+            return;
+        }
+        this[page]();
+    }
 }
